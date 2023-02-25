23b9c681480b707cc09daf802579d9ade0df81c1
Patrick Hao <patrick.hao95@gmail.com>
2023-02-25 14:42:10 +1100

    Created basic tests for bcm_timer_regs.

--- mmc/include/bcm_timer_regs.h
+++ mmc/include/bcm_timer_regs.h
@@ -9,11 +9,11 @@
 
 typedef struct bcm_timer_regs bcm_timer_regs_t;
 struct __attribute__((__packed__, aligned(4))) bcm_timer_regs {
-    uint32_t control_status;                                            // 0x00
-    uint32_t counter_lo;                                                // 0x04
-    uint32_t counter_hi;                                                // 0x08
-    uint32_t compare0;                                                // 0x0C
-    uint32_t compare1;                                                // 0x10
-    uint32_t compare2;                                                // 0x14
-    uint32_t compare3;                                                // 0x18
+    uint32_t control_status;  // 0x00
+    uint32_t counter_lo;      // 0x04
+    uint32_t counter_hi;      // 0x08
+    uint32_t compare0;        // 0x0C
+    uint32_t compare1;        // 0x10
+    uint32_t compare2;        // 0x14
+    uint32_t compare3;        // 0x18
 };
---
+++ mmc_test/unit/timer_driver/test_bcm_timer_regs.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "bcm_timer_regs.h"
+}
+
+TEST(test_bcm_timer_regs, regs_should_have_the_correct_offset) {
+    /* Create a new register struct on the stack. */
+    bcm_timer_regs_t regs = {};
+    /* Below is the base register. */
+    uintptr_t base = (uintptr_t) &regs;
+
+    ASSERT_EQ(base, (uintptr_t) &regs.control_status);
+    ASSERT_EQ(base + 0x4, (uintptr_t) &regs.counter_lo);
+    ASSERT_EQ(base + 0x8, (uintptr_t) &regs.counter_hi);
+    ASSERT_EQ(base + 0xc, (uintptr_t) &regs.compare0);
+    ASSERT_EQ(base + 0x10, (uintptr_t) &regs.compare1);
+    ASSERT_EQ(base + 0x14, (uintptr_t) &regs.compare2);
+    ASSERT_EQ(base + 0x18, (uintptr_t) &regs.compare3);
+}
