0e491beb385109d52dffe916e8765ae0bf37fb25
Patrick Hao <patrick.hao95@gmail.com>
2023-03-09 16:59:53 +1100

    Refactored setting the clock to 25000000.

--- mmc/src/sd.c
+++ mmc/src/sd.c
@@ -474,7 +474,12 @@ int sd_init(bcm_emmc_regs_t *regs, sdcard_t *sd) {
     uart_puts("\n");
     if (sd_err) return sd_err;
 
-    if ((r = sd_clk(25000000))) return r;
+//    if ((r = sd_clk(25000000))) return r;
+    res = sdhci_set_sd_clock(global_regs, 25000000);
+    if (result_is_err(res)) {
+        result_printf(res);
+        return -1;
+    }
 
     sd_cmd(CMD_CARD_SELECT, sd_rca);
     if (sd_err) return sd_err;
