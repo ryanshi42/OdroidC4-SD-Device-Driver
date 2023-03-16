f9aa266ab3b109e6481be7cb2a8e6c4ccb6066ab
Patrick Hao <patrick.hao95@gmail.com>
2023-03-16 11:20:12 +1100

    Refactored comments in gpio_driver.c.

--- mmc/src/gpio_driver.c
+++ mmc/src/gpio_driver.c
@@ -30,13 +30,13 @@ result_t gpio_driver_fix_resistor(
     if (gpio_fix_resistor > 3) {
         return result_err("Invalid GPIO fix resistor in gpio_driver_fix_resistor().");
     }
-    uint32_t regnum = pin_no / 32;								// Create register number
-    uint32_t bit = 1 << (pin_no % 32);							// Create mask bit
-    bcm_gpio_regs->GPPUD = gpio_fix_resistor;										// Set fixed resistor request to PUD register
+    uint32_t regnum = pin_no / 32; /* Create register number */
+    uint32_t bit = 1 << (pin_no % 32); /* Create mask bit */
+    bcm_gpio_regs->GPPUD = gpio_fix_resistor; /* Set fixed resistor request to PUD register */
     sleep_cyc(150); /* Sleep for 150 cycles. */
-    bcm_gpio_regs->GPPUDCLK[regnum] = bit;								// Set the PUD clock bit register
+    bcm_gpio_regs->GPPUDCLK[regnum] = bit; /* Set the PUD clock bit register */
     sleep_cyc(150); /* Sleep for 150 cycles. */
-    bcm_gpio_regs->GPPUD = 0;											// Clear GPIO resistor setting
-    bcm_gpio_regs->GPPUDCLK[regnum] = 0;									// Clear PUDCLK from GPIO
+    bcm_gpio_regs->GPPUD = 0; /* Clear GPIO resistor setting */
+    bcm_gpio_regs->GPPUDCLK[regnum] = 0; /* Clear PUDCLK from GPIO */
     return result_ok();
 }
