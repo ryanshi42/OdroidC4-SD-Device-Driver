3ab7e874a91329c12327d858d9579d5569173763
Patrick Hao <patrick.hao95@gmail.com>
2023-03-16 13:52:48 +1100

    Refactored GPIO fix resistor into an enum.

--- mmc/include/gpio_fix_resistor.h
+++ mmc/include/gpio_fix_resistor.h
@@ -5,9 +5,10 @@
 /*
  * ENUMERATED GPIO FIX RESISTOR ... BCM2835.PDF MANUAL see page 101
  */
-/* In binary so any error is obvious */
-#define NO_RESISTOR		0b00							// 0
-#define PULLUP			0b01							// 1
-#define PULLDOWN		0b10							// 2
-typedef uint8_t gpio_fix_resistor_t;
+enum gpio_fix_resister {
+    NO_RESISTOR = 0b00, // 0
+    PULLUP = 0b01,      // 1
+    PULLDOWN = 0b10     // 2
+};
+typedef enum gpio_fix_resister gpio_fix_resistor_t;
 
