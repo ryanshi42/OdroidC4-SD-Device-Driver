06dd514d15ea83422318b9938c00e7ddb4713c49
Patrick Hao <patrick.hao95@gmail.com>
2023-03-16 13:50:03 +1100

    Changed GPIO mode to an enum instead of #defines.

--- mmc/include/gpio_mode.h
+++ mmc/include/gpio_mode.h
@@ -5,12 +5,14 @@
 /*
  * ENUMERATED FSEL REGISTERS ... BCM2835.PDF MANUAL see page 92		}
  */
-#define GPIO_INPUT		0b000							// 0
-#define GPIO_OUTPUT		0b001							// 1
-#define GPIO_ALTFUNC5	0b010							// 2
-#define GPIO_ALTFUNC4	0b011							// 3
-#define GPIO_ALTFUNC0	0b100							// 4
-#define	GPIO_ALTFUNC1	0b101							// 5
-#define	GPIO_ALTFUNC2	0b110							// 6
-#define GPIO_ALTFUNC3	0b111							// 7
-typedef uint8_t gpio_mode_t;
+enum gpio_mode {
+    GPIO_INPUT = 0b000,     // 0
+    GPIO_OUTPUT = 0b001,    // 1
+    GPIO_ALTFUNC5 = 0b010,  // 2
+    GPIO_ALTFUNC4 = 0b011,  // 3
+    GPIO_ALTFUNC0 = 0b100,  // 4
+    GPIO_ALTFUNC1 = 0b101,  // 5
+    GPIO_ALTFUNC2 = 0b110,  // 6
+    GPIO_ALTFUNC3 = 0b111   // 7
+};
+typedef enum gpio_mode gpio_mode_t;
