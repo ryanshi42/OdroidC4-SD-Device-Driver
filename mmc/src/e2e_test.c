f0d77cabab8b6e358f64ec828241a07e97f3cdb0
Patrick Hao <patrick.hao95@gmail.com>
2023-03-17 11:44:14 +1100

    Added some more helpful printing to e2e_test.c.

--- mmc/src/e2e_test.c
+++ mmc/src/e2e_test.c
@@ -1,6 +1,8 @@
 #include "e2e_test.h"
 
 result_t e2e_test_sleep() {
+    log_info("Starting e2e_test_sleep():");
+
     uint64_t start_ticks, finish_ticks, delta_ticks;
     start_ticks = clock_getticks();
     usleep(1000000);
@@ -16,5 +18,6 @@ result_t e2e_test_sleep() {
     log_info("delta_ticks (sleep_cyc): %lu", delta_ticks);
     assert(delta_ticks <= 10);
 
+    log_info("Finished e2e_test_sleep():");
     return result_ok();
 }
