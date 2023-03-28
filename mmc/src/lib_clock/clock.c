dedceb4e3f8dbf754753bc5e5e6c80a5b5f60447
Patrick Hao <patrick.hao95@gmail.com>
2023-03-28 21:19:02 +1100

    Cleaned up clock.c by removing unecessary code.

--- mmc/src/lib_clock/clock.c
+++ mmc/src/lib_clock/clock.c
@@ -10,18 +10,6 @@ result_t clock_init(timer_client_t *timer_client) {
     return result_ok();
 }
 
-//
-//int clock_gettime(clockid_t clock_id, struct timespec *tp) {
-//    memset(tp, 0, sizeof(struct timespec));
-//    if (clock_id != CLOCK_MONOTONIC) {
-//        return -1;
-//    }
-//    unsigned int micros = get_system_timer();
-//    tp->tv_sec = micros / US_IN_S;
-//    tp->tv_nsec = (micros % US_IN_S) * NS_IN_US;
-//    return 0;
-//}
-
 uint64_t clock_getticks(void) {
     uint64_t num_ticks = 0;
     result_t result = timer_client_get_num_ticks(
