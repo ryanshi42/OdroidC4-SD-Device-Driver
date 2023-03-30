e970f9129ae97af0f7e356ebbcaceaa6488ca123
Patrick Hao <patrick.hao95@gmail.com>
2023-03-30 16:00:14 +1100

    Minor reformatting of csd.h.

--- mmc/include/csd.h
+++ mmc/include/csd.h
@@ -11,8 +11,8 @@ typedef enum csd_structure csd_structure_t;
 enum file_format {
     FAT_PARTITION_TABLE = 0,      // enum SD card is FAT with partition table
     FAT_NO_PARTITION_TABLE = 1,   // enum SD card is FAT with no partition table
-    FS_UNIVERSAL = 2,             // enum	SD card file system is universal
-    FS_OTHER = 3,                 // enum	SD card file system is other
+    FS_UNIVERSAL = 2,             // enum SD card file system is universal
+    FS_OTHER = 3,                 // enum SD card file system is other
 };
 typedef enum file_format file_format_t;
 
@@ -28,7 +28,7 @@ struct __attribute__((__packed__, aligned(4))) csd {
     union {
         struct __attribute__((__packed__, aligned(1))) {
             csd_structure_t csd_structure: 2;  // @127-126	CSD Structure Version as on SD CSD bits
-            unsigned spec_vers: 6;             // @125-120 CSD version as on SD CSD bits
+            unsigned spec_vers: 6;             // @125-120  CSD version as on SD CSD bits
             uint8_t taac;                      // @119-112	taac as on SD CSD bits
             uint8_t nsac;                      // @111-104	nsac as on SD CSD bits
             uint8_t tran_speed;                // @103-96	trans_speed as on SD CSD bits
@@ -39,11 +39,11 @@ struct __attribute__((__packed__, aligned(4))) csd {
         struct __attribute__((__packed__, aligned(1))) {
             unsigned ccc: 12;                 // @95-84	ccc as on SD CSD bits
             unsigned read_bl_len: 4;          // @83-80	read_bl_len on SD CSD bits
-            unsigned read_bl_partial: 1;      // @79		read_bl_partial as on SD CSD bits
-            unsigned write_blk_misalign: 1;   // @78		write_blk_misalign as on SD CSD bits
-            unsigned read_blk_misalign: 1;    // @77		read_blk_misalign as on SD CSD bits
-            unsigned dsr_imp: 1;              // @76		dsr_imp as on SD CSD bits
-            unsigned c_size: 12;              // @75-64   Version 1 C_Size as on SD CSD bits
+            unsigned read_bl_partial: 1;      // @79	read_bl_partial as on SD CSD bits
+            unsigned write_blk_misalign: 1;   // @78	write_blk_misalign as on SD CSD bits
+            unsigned read_blk_misalign: 1;    // @77	read_blk_misalign as on SD CSD bits
+            unsigned dsr_imp: 1;              // @76	dsr_imp as on SD CSD bits
+            unsigned c_size: 12;              // @75-64 Version 1 C_Size as on SD CSD bits
         };
         uint32_t raw32_1;                     // @0-31	Union to access 32 bits as a uint32_t
     };
@@ -69,18 +69,18 @@ struct __attribute__((__packed__, aligned(4))) csd {
     union {
         struct __attribute__((__packed__, aligned(1))) {
             unsigned wp_grp_size: 7;          // @38-32	wp_grp_size as on SD CSD bits
-            unsigned wp_grp_enable: 1;        // @31		wp_grp_enable as on SD CSD bits
-            unsigned reserved2: 2;            // @30-29 	Write as zero read as don't care
+            unsigned wp_grp_enable: 1;        // @31	wp_grp_enable as on SD CSD bits
+            unsigned reserved2: 2;            // @30-29 Write as zero read as don't care
             unsigned r2w_factor: 3;           // @28-26	r2w_factor as on SD CSD bits
             unsigned write_bl_len: 4;         // @25-22	write_bl_len as on SD CSD bits
-            unsigned write_bl_partial: 1;     // @21		write_bl_partial as on SD CSD bits
+            unsigned write_bl_partial: 1;     // @21	write_bl_partial as on SD CSD bits
             unsigned default_ecc: 5;          // @20-16	default_ecc as on SD CSD bits
-            unsigned file_format_grp: 1;      // @15		file_format_grp as on SD CSD bits
-            unsigned copy: 1;                 // @14		copy as on SD CSD bits
-            unsigned perm_write_protect: 1;   // @13		perm_write_protect as on SD CSD bits
-            unsigned tmp_write_protect: 1;    // @12		tmp_write_protect as on SD CSD bits
+            unsigned file_format_grp: 1;      // @15	file_format_grp as on SD CSD bits
+            unsigned copy: 1;                 // @14	copy as on SD CSD bits
+            unsigned perm_write_protect: 1;   // @13	perm_write_protect as on SD CSD bits
+            unsigned tmp_write_protect: 1;    // @12	tmp_write_protect as on SD CSD bits
             file_format_t file_format: 2;     // @11-10	File format as on SD CSD bits
-            unsigned ecc: 2;                  // @9-8		ecc as on SD CSD bits
+            unsigned ecc: 2;                  // @9-8	ecc as on SD CSD bits
             unsigned reserved3: 1;            // 1 spare bit unused
         };
         uint32_t raw32_3;                     // @0-31	Union to access 32 bits as a uint32_t
