14c30ffe31f5af364c649a84105990e743656d7e
Patrick Hao <patrick.hao95@gmail.com>
2023-05-11 09:38:30 +1000

    Created and tested blk_response_queue.

--- mmc/src/lib_blk_sddf/blk_response_queue.c
+++ mmc/src/lib_blk_sddf/blk_response_queue.c
@@ -1,2 +1,150 @@
 #include "blk_response_queue.h"
 
+blk_response_queue_result_t blk_response_queue_init(
+        blk_response_queue_t *queue,
+        size_t req_queue_region_size
+) {
+    if (queue == NULL) {
+        return ERR_NULL_BLK_RESPONSE_QUEUE;
+    }
+    if (req_queue_region_size < sizeof(*queue)) {
+        return ERR_BLK_RESPONSE_QUEUE_REGION_TOO_SMALL;
+    }
+    size_t const num_unused_bytes = (req_queue_region_size - sizeof(*queue));
+    if ((num_unused_bytes / sizeof(blk_response_t)) > 0) {
+        return ERR_BLK_RESPONSE_QUEUE_REGION_TOO_LARGE;
+    }
+    /* Clear the `queue` data structure, which initialises `head_idx` and
+     * `tail_idx` to zero. */
+    memset(queue, 0, sizeof(*queue));
+    return OK_BLK_RESPONSE_QUEUE;
+}
+
+blk_response_queue_result_t blk_response_queue_capacity(
+        blk_response_queue_t *queue,
+        size_t *ret_val
+) {
+    if (queue == NULL) {
+        return ERR_NULL_BLK_RESPONSE_QUEUE;
+    }
+    if (ret_val == NULL) {
+        return ERR_NULL_RET_VAL_PTR_PASSED_TO_BLK_RESPONSE_QUEUE_FN;
+    }
+    *ret_val = MAX_NUM_BLK_RESPONSES - 1;
+    return OK_BLK_RESPONSE_QUEUE;
+}
+
+blk_response_queue_result_t blk_response_queue_size(
+        blk_response_queue_t *queue,
+        size_t *ret_val
+) {
+    if (queue == NULL) {
+        return ERR_NULL_BLK_RESPONSE_QUEUE;
+    }
+    if (ret_val == NULL) {
+        return ERR_NULL_RET_VAL_PTR_PASSED_TO_BLK_RESPONSE_QUEUE_FN;
+    }
+    size_t const head = queue->head_idx;
+    size_t const tail = queue->tail_idx;
+    if (tail >= head) {
+        *ret_val = (tail - head);
+    } else {
+        *ret_val = ((tail + MAX_NUM_BLK_RESPONSES) - head);
+    }
+    return OK_BLK_RESPONSE_QUEUE;
+}
+
+blk_response_queue_result_t blk_response_queue_is_empty(
+        blk_response_queue_t *queue,
+        bool *ret_val
+) {
+    if (queue == NULL) {
+        return ERR_NULL_BLK_RESPONSE_QUEUE;
+    }
+    if (ret_val == NULL) {
+        return ERR_NULL_RET_VAL_PTR_PASSED_TO_BLK_RESPONSE_QUEUE_FN;
+    }
+    *ret_val = (queue->head_idx == queue->tail_idx);
+    return OK_BLK_RESPONSE_QUEUE;
+}
+
+blk_response_queue_result_t blk_response_queue_is_full(
+        blk_response_queue_t *queue,
+        bool *ret_val
+) {
+    if (queue == NULL) {
+        return ERR_NULL_BLK_RESPONSE_QUEUE;
+    }
+    if (ret_val == NULL) {
+        return ERR_NULL_RET_VAL_PTR_PASSED_TO_BLK_RESPONSE_QUEUE_FN;
+    }
+    size_t const head = queue->head_idx;
+    size_t const tail = queue->tail_idx;
+    size_t const unused_slots = 1;
+    *ret_val = (head == ((tail + unused_slots) % MAX_NUM_BLK_RESPONSES));
+    return OK_BLK_RESPONSE_QUEUE;
+}
+
+blk_response_queue_result_t blk_response_queue_enqueue(
+        blk_response_queue_t *queue,
+        blk_response_t *val
+) {
+    if (queue == NULL) {
+        return ERR_NULL_BLK_RESPONSE_QUEUE;
+    }
+    if (val == NULL) {
+        return ERR_NULL_BLK_RESPONSE;
+    }
+    bool is_full = false;
+    blk_response_queue_result_t const res = blk_response_queue_is_full(queue, &is_full);
+    if (res != OK_BLK_RESPONSE_QUEUE) {
+        return res;
+    }
+    if (is_full) {
+        return ERR_BLK_RESPONSE_QUEUE_FULL;
+    }
+    /* Copy everything in `val` into the next available slot in the response ring
+     * buffer. */
+    memcpy(&queue->responses[queue->tail_idx], val, sizeof(*val));
+    /* We place a memory barrier here to ensure that the instructions that
+     * update the tail index are guaranteed to occur after the buffer has been
+     * enqueued onto `queue->req_bufs`. */
+    THREAD_MEMORY_RELEASE();
+    /* Update the tail index. */
+    queue->tail_idx = (queue->tail_idx + 1) % MAX_NUM_BLK_RESPONSES;
+    return OK_BLK_RESPONSE_QUEUE;
+}
+
+blk_response_queue_result_t blk_response_queue_dequeue(
+        blk_response_queue_t *queue,
+        blk_response_t *ret_val
+) {
+    if (queue == NULL) {
+        return ERR_NULL_BLK_RESPONSE_QUEUE;
+    }
+    if (ret_val == NULL) {
+        return ERR_NULL_RET_VAL_PTR_PASSED_TO_BLK_RESPONSE_QUEUE_FN;
+    }
+    /* Return an error if the ring buffer is empty and there is nothing to
+     * dequeue. */
+    bool is_empty = false;
+    blk_response_queue_result_t const res = blk_response_queue_is_empty(queue, &is_empty);
+    if (res != OK_BLK_RESPONSE_QUEUE) {
+        return res;
+    }
+    if (is_empty) {
+        return ERR_BLK_RESPONSE_QUEUE_EMPTY;
+    }
+    /* Copy everything from the first slot into `ret_val`. */
+    memcpy(ret_val, &queue->responses[queue->head_idx], sizeof(*ret_val));
+    /* We place a memory barrier here to ensure that the instructions that
+     * update the head index are guaranteed to occur after the buffer has been
+     * dequeued from `queue->req_bufs`. */
+    THREAD_MEMORY_RELEASE();
+    /* Update the head index. */
+    queue->head_idx = (queue->head_idx + 1) % MAX_NUM_BLK_RESPONSES;
+    return OK_BLK_RESPONSE_QUEUE;
+}
+
+
+
--- mmc/src/lib_blk_sddf/blk_response_queue.h
+++ mmc/src/lib_blk_sddf/blk_response_queue.h
@@ -11,5 +11,139 @@
 #include "fence.h"
 #include "blk_response.h"
 
+#define MAX_NUM_BLK_RESPONSES (512)
+
+typedef struct blk_response_queue blk_response_queue_t;
+struct blk_response_queue {
+    size_t head_idx; /* We dequeue from the head. This is also known as the
+    "read index". */
+    size_t tail_idx; /* We enqueue onto the tail. This is also known as the
+    "write index". */
+    blk_response_t responses[MAX_NUM_BLK_RESPONSES];
+};
+
+enum blk_response_queue_result {
+    OK_BLK_RESPONSE_QUEUE = 0,
+    ERR_NULL_BLK_RESPONSE_QUEUE = -1, /* A NULL `queue` pointer was passed in. */
+    ERR_BLK_RESPONSE_QUEUE_REGION_TOO_SMALL = -2, /* The shared memory region
+    reserved for this ring buffer is too small. To remedy this, you can either
+    increase the shared memory region reserved for the `blk_response
+    structure or reduce the size of `MAX_NUM_BLK_RESPONSES`. */
+    ERR_BLK_RESPONSE_QUEUE_REGION_TOO_LARGE = -3, /* The shared memory region
+    reserved for this ring buffer is wastefully large. To remedy this, you can
+    either reduce the shared memory region reserved for the `blk_response
+    or increase the size of `MAX_NUM_BLK_RESPONSES`. */
+    ERR_NULL_RET_VAL_PTR_PASSED_TO_BLK_RESPONSE_QUEUE_FN = -4, /* A NULL `ret_val` pointer
+    was passed into a `blk_response_queue` function. */
+    ERR_NULL_BLK_RESPONSE = -5, /* A NULL `request
+    blk_response_queue_enqueue */
+    ERR_BLK_RESPONSE_QUEUE_FULL = -6, /* The ring buffer is full. */
+    ERR_BLK_RESPONSE_QUEUE_EMPTY = -7, /* The ring buffer is empty. */
+};
+typedef enum blk_response_queue_result blk_response_queue_result_t;
+
+/**
+ * Initializes a Request Ring Buffer.
+ * @param queue
+ * @param request_queue_region_size Size of the shared memory region reserved for
+ * this Request Ring Buffer in bytes.
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_init(
+        blk_response_queue_t *queue,
+        size_t request_queue_region_size
+);
+
+/**
+ * Returns the number of Requests that can be enqueued onto the Request Ring
+ * Buffer.
+ * @param queue
+ * @param ret_val
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_capacity(
+        blk_response_queue_t *queue,
+        size_t *ret_val
+);
+
+/**
+ * Returns the number of Requests that are currently enqueued onto the Request
+ * Ring Buffer.
+ * @param queue
+ * @param ret_val
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_size(
+        blk_response_queue_t *queue,
+        size_t *ret_val
+);
+
+/**
+ * Enqueues a Request onto the Request Ring Buffer.
+ * @param queue
+ * @param req The Request to enqueue.
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_enqueue(
+        blk_response_queue_t *queue,
+        blk_response_t *req
+);
+
+/**
+ * Dequeues a Request from the Request Ring Buffer.
+ * @param queue
+ * @param ret_val The dequeued Request.
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_dequeue(
+        blk_response_queue_t *queue,
+        blk_response_t *ret_val
+);
+
+
+/**
+ * Returns True if the ring buffer is empty, False otherwise.
+ * @param queue
+ * @param ret_val
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_is_empty(
+        blk_response_queue_t *queue,
+        bool *ret_val
+);
+
+/**
+ * Returns True if the ring buffer is full, False otherwise.
+ * @param queue
+ * @param ret_val
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_is_full(
+        blk_response_queue_t *queue,
+        bool *ret_val
+);
+
+/**
+ * Enqueues the `blk_response` onto the ring buffer.
+ * @param queue
+ * @param val
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_enqueue(
+        blk_response_queue_t *queue,
+        blk_response_t *val
+);
+
+/**
+ * Dequeues the `blk_response` from the ring buffer.
+ * @param queue
+ * @param ret_val
+ * @return
+ */
+blk_response_queue_result_t blk_response_queue_dequeue(
+        blk_response_queue_t *queue,
+        blk_response_t *ret_val
+);
+
 
 
