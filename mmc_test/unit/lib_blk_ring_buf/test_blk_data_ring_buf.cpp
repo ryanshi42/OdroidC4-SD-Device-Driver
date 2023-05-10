1238e0d4a3e97bce11161cef3fa2f8c5698f2af4
Patrick Hao <patrick.hao95@gmail.com>
2023-05-10 21:03:05 +1000

    Wrote more tests for blk_data_ring_buf.c.

--- mmc_test/unit/lib_blk_ring_buf/test_blk_data_ring_buf.cpp
+++ mmc_test/unit/lib_blk_ring_buf/test_blk_data_ring_buf.cpp
@@ -19,7 +19,10 @@ TEST(test_blk_data_ring_buf, init_should_compute_correct_capacity) {
             )
     );
     size_t capacity;
-    ASSERT_EQ(0, blk_data_ring_buf_capacity(&ring_buf, &capacity));
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_capacity(&ring_buf, &capacity)
+    );
     ASSERT_EQ(capacity, 10);
 }
 
@@ -38,25 +41,40 @@ TEST(test_blk_data_ring_buf, init_should_completely_fill_ring_buffer) {
     );
     /* Queue should initially be full. */
     bool is_full;
-    ASSERT_EQ(0, blk_data_ring_buf_is_full(&ring_buf, &is_full));
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_is_full(&ring_buf, &is_full)
+    );
     ASSERT_TRUE(is_full);
     /* Second check the Queue is full. */
     size_t num_data_bufs;
-    ASSERT_EQ(0, blk_data_ring_buf_size(&ring_buf, &num_data_bufs));
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_size(&ring_buf, &num_data_bufs)
+    );
     ASSERT_EQ(num_data_bufs, 10);
     /* There should only be 10 buffers in the queue. */
     for (size_t i = 0; i < 10; i++) {
         blk_data_buf_t data_buf = {0};
-        ASSERT_EQ(0, blk_data_ring_buf_dequeue(&ring_buf, &data_buf));
+        ASSERT_EQ(
+                OK_BLK_DATA_RING_BUF,
+                blk_data_ring_buf_dequeue(&ring_buf, &data_buf)
+        );
         ASSERT_EQ(data_buf.buf, data_region_vaddr + (i * 10));
         ASSERT_EQ(data_buf.buf_size, 10);
     }
     /* Queue should be empty. */
     bool is_empty;
-    ASSERT_EQ(0, blk_data_ring_buf_is_empty(&ring_buf, &is_empty));
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_is_empty(&ring_buf, &is_empty)
+    );
     ASSERT_TRUE(is_empty);
     /* Second check the Queue is empty. */
-    ASSERT_EQ(0, blk_data_ring_buf_size(&ring_buf, &num_data_bufs));
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_size(&ring_buf, &num_data_bufs)
+    );
     ASSERT_EQ(num_data_bufs, 0);
 }
 
@@ -166,11 +184,208 @@ TEST(test_blk_data_ring_buf, capacity_should_reject_null_capacity) {
     );
 }
 
-/* TODO: Write test for blk_data_ring_buf_is_full(). */
+/* is_full() */
 
-/* TODO: Write test for blk_data_ring_buf_enqueue(). */
+TEST(test_blk_data_ring_buf, is_full_should_return_true_if_queue_is_full_and_false_otherwise) {
+    blk_data_ring_buf_t ring_buf = {0};
+    char data_region[100] = {0};
+    uintptr_t data_region_vaddr = (uintptr_t) data_region;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_init(
+                    &ring_buf,
+                    data_region_vaddr,
+                    100,
+                    10
+            )
+    );
+    /* Queue should initially be full. */
+    bool is_full;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_is_full(&ring_buf, &is_full)
+    );
+    ASSERT_TRUE(is_full);
+    /* We dequeue a single buffer. */
+    blk_data_buf_t data_buf = {0};
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_dequeue(
+                    &ring_buf,
+                    &data_buf
+            )
+    );
+    /* Queue should no longer be full. */
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_is_full(&ring_buf, &is_full)
+    );
+    ASSERT_FALSE(is_full);
+}
 
-/* TODO: Write test for blk_data_ring_buf_dequeue(). */
+/* is_empty() */
+
+TEST(test_blk_data_ring_buf, is_empty_should_return_true_if_queue_is_empty_and_false_otherwise) {
+    blk_data_ring_buf_t ring_buf = {0};
+    char data_region[100] = {0};
+    uintptr_t data_region_vaddr = (uintptr_t) data_region;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_init(
+                    &ring_buf,
+                    data_region_vaddr,
+                    100,
+                    10
+            )
+    );
+    /* Get capacity of ring buffer. */
+    size_t capacity;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_capacity(&ring_buf, &capacity)
+    );
+    /* We dequeue until there is only a single buffer left in the queue. */
+    blk_data_buf_t data_buf = {};
+    for (size_t i = 0; i < capacity - 1; i++) {
+        ASSERT_EQ(
+                OK_BLK_DATA_RING_BUF,
+                blk_data_ring_buf_dequeue(
+                        &ring_buf,
+                        &data_buf
+                )
+        );
+    }
+    /* Queue should not be empty. */
+    bool is_empty;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_is_empty(&ring_buf, &is_empty)
+    );
+    ASSERT_FALSE(is_empty);
+    /* We dequeue once more. */
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_dequeue(
+                    &ring_buf,
+                    &data_buf
+            )
+    );
+    /* Queue should be empty now. */
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_is_empty(&ring_buf, &is_empty)
+    );
+    ASSERT_TRUE(is_empty);
+}
+
+/* dequeue() */
+
+TEST(test_blk_data_ring_buf, dequeue_should_dequeue_capacity_number_of_data_bufs) {
+    blk_data_ring_buf_t ring_buf = {0};
+    char data_region[100] = {0};
+    uintptr_t data_region_vaddr = (uintptr_t) data_region;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_init(
+                    &ring_buf,
+                    data_region_vaddr,
+                    100,
+                    10
+            )
+    );
+    /* Get capacity of ring buffer. */
+    size_t capacity;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_capacity(&ring_buf, &capacity)
+    );
+    /* Queue is initially full so we must dequeue all buffers first to test enqueue. */
+    blk_data_buf_t data_buf = {};
+    for (size_t i = 0; i < capacity; i++) {
+        ASSERT_EQ(
+                OK_BLK_DATA_RING_BUF,
+                blk_data_ring_buf_dequeue(
+                        &ring_buf,
+                        &data_buf
+                )
+        );
+    }
+    /* Queue should be empty. */
+    bool is_empty;
+    ASSERT_EQ(OK_BLK_DATA_RING_BUF, blk_data_ring_buf_is_empty(&ring_buf, &is_empty));
+    ASSERT_TRUE(is_empty);
+    /* Queue is empty so user should not be able to dequeue from the buffer. */
+    ASSERT_EQ(
+            ERR_BLK_DATA_RING_BUF_EMPTY,
+            blk_data_ring_buf_dequeue(
+                    &ring_buf,
+                    &data_buf
+            )
+    );
+}
+
+/* enqueue() */
+
+TEST(test_blk_data_ring_buf, enqueue_should_enqueue_capacity_number_of_data_bufs) {
+    blk_data_ring_buf_t ring_buf = {0};
+    char data_region[100] = {0};
+    uintptr_t data_region_vaddr = (uintptr_t) data_region;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_init(
+                    &ring_buf,
+                    data_region_vaddr,
+                    100,
+                    10
+            )
+    );
+    /* Get capacity of ring buffer. */
+    size_t capacity;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_capacity(&ring_buf, &capacity)
+    );
+    /* Queue is initially full so we must dequeue all buffers first to test enqueue. */
+    blk_data_buf_t data_buf = {};
+    for (size_t i = 0; i < capacity; i++) {
+        ASSERT_EQ(
+                OK_BLK_DATA_RING_BUF,
+                blk_data_ring_buf_dequeue(
+                        &ring_buf,
+                        &data_buf
+                )
+        );
+    }
+    /* Queue should be empty. */
+    bool is_empty;
+    ASSERT_EQ(OK_BLK_DATA_RING_BUF, blk_data_ring_buf_is_empty(&ring_buf, &is_empty));
+    ASSERT_TRUE(is_empty);
+    /* Enqueue `capacity` number of buffers. */
+    for (size_t i = 0; i < capacity; i++) {
+        ASSERT_EQ(
+                OK_BLK_DATA_RING_BUF,
+                blk_data_ring_buf_enqueue(
+                        &ring_buf,
+                        &data_buf
+                )
+        );
+    }
+    /* Queue should be full. */
+    bool is_full;
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_is_full(&ring_buf, &is_full)
+    );
+    ASSERT_TRUE(is_full);
+    /* Queue is full so user should not be able to enqueue onto the buffer. */
+    ASSERT_EQ(
+            ERR_BLK_DATA_RING_BUF_FULL,
+            blk_data_ring_buf_enqueue(
+                    &ring_buf,
+                    &data_buf
+            )
+    );
+}
 
 /* size() */
 
@@ -179,6 +394,9 @@ TEST(test_blk_data_ring_buf, size_should_return_correct_size_if_tail_less_than_h
     ring_buf.head_idx = MAX_NUM_BLK_DATA_BUFS - 5;
     ring_buf.tail_idx = 5;
     size_t size;
-    ASSERT_EQ(0, blk_data_ring_buf_size(&ring_buf, &size));
+    ASSERT_EQ(
+            OK_BLK_DATA_RING_BUF,
+            blk_data_ring_buf_size(&ring_buf, &size)
+    );
     ASSERT_EQ(size, 10);
 }
